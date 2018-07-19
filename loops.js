34382474d9dfa66ca4a32c1a2bfc3ccafd29b59c
joe fisher <jhanajon@gmail.com>
2018-07-19 17:02:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,6 +17,7 @@ array.push(`"I am ${i} strange loops."`);
 function whileLoop(n) {
   // while (n = 0; n > 0 ;  --n)
   while (n > 0) {
-  console.log(--n('done'))
+  console.log(--n)
 }
+return 'done'
 }
