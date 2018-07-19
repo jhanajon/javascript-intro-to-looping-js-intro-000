5e12e3c157841f05234c608f669b24d0f46cea3e
joe fisher <jhanajon@gmail.com>
2018-07-19 17:47:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -35,7 +35,8 @@ function doWhileLoop(array) {
   ///array.length > 0 && maybeTrue()
   return array.length > 0 && maybeTrue() {
     do {
-    array.pop()
+      console.log(`'hey what's wrong'`)
+    // array.pop()
     }
   }
 } while (doWhileLoop())
