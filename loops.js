d01912323214591efb8b91b46be530fd20fb9a3e
joe fisher <jhanajon@gmail.com>
2018-07-19 16:23:10 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,21 @@
+//The for loop worked in the console to log the results, however, the task was here
+//to push it (add) to the array that requires the array.push
+//there's no need to redefine the array since it can refer to any array (abstracted)
+
+function forLoop(array) {
+for (var i = 0; i < 25; i++) {
+if (i === 1) {
+array.push( "I am 1 strange loop." );
+     }
+else {
+array.push(`"I am ${i} strange loops."`);
+     }
+     }
+     return array
+   }
+
+function whileLoop(n) {
+  while (n > 0) {
+  console.log('done')
+}
+}
